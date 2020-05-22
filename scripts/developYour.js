b395a8780bdb9a66d019a27b4b21ac181ef3bfde
Littl3S3al <lauseal@gmail.com>
2020-05-22 16:31:30 +0200

    about services work

--- scripts/developYour.js
+++ scripts/developYour.js
@@ -1,10 +1,12 @@
 const showText = document.querySelector('.show-text');
 let i = 0;
 
-setInterval(() => {
-    if(i === textFields.length){
-        i =0;
-    }
-    showText.innerText = textFields[i];
-    i++;
-}, 300)
+if(showText){
+    setInterval(() => {
+        if(i === textFields.length){
+            i =0;
+        }
+        showText.innerText = textFields[i];
+        i++;
+    }, 300)
+}
